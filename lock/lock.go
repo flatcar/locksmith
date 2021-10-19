c00334874ec4e4a6d1f561337b18956265424f59
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-10-19 20:40:17 +0530

    Remove the store, Get, SetMax function in lock/lock.go

--- lock/lock.go
+++ lock/lock.go
@@ -25,52 +25,6 @@ func New(id string, client LockClient) (lock *Lock) {
 	return &Lock{id, client}
 }
 
-func (l *Lock) store(f func(*Semaphore) error) (err error) {
-	sem, err := l.client.Get()
-	if err != nil {
-		return err
-	}
-
-	if err := f(sem); err != nil {
-		return err
-	}
-
-	err = l.client.Set(sem)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Get returns the current semaphore value
-// if the underlying client returns an error, Get passes it through
-func (l *Lock) Get() (sem *Semaphore, err error) {
-	sem, err = l.client.Get()
-	if err != nil {
-		return nil, err
-	}
-
-	return sem, nil
-}
-
-// SetMax sets the maximum number of holders the semaphore will allow
-// it returns the current semaphore and the previous maximum
-// if there is a problem getting or setting the semaphore, this function will
-// pass on errors from the underlying client
-func (l *Lock) SetMax(max int) (sem *Semaphore, oldMax int, err error) {
-	var (
-		semRet *Semaphore
-		old    int
-	)
-
-	return semRet, old, l.store(func(sem *Semaphore) error {
-		old = sem.Max
-		semRet = sem
-		return sem.SetMax(max)
-	})
-}
-
 // Lock adds this lock id as a holder to the semaphore
 // it will return an error if there is a problem getting or setting the
 // semaphore, or if the maximum number of holders has been reached, or if a lock
